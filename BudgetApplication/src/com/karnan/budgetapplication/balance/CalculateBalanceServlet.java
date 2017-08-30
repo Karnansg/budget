ae4d2eae91bdeece5356de98f7496648e25e94d5
Karnansg <karnan75@gmail.com>
2017-08-31 02:23:25 +0800

    ObjectMapper instantiation and try catch block, commented out.

--- BudgetApplication/src/com/karnan/budgetapplication/balance/CalculateBalanceServlet.java
+++ BudgetApplication/src/com/karnan/budgetapplication/balance/CalculateBalanceServlet.java
@@ -1,5 +1,6 @@
 package com.karnan.budgetapplication.balance;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -10,6 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpSession;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.karnan.budgetapplication.business.Amount;
 import com.karnan.budgetapplication.business.Compute;
 import com.karnan.budgetapplication.business.Expenditure;
@@ -43,13 +47,28 @@ public class CalculateBalanceServlet extends HttpServlet {
 		//create the expenditure object
 		String userId = "1";
 		Expenditure expenditure = new Expenditure();
+		Expenditure expenditureData = new Expenditure();
 		expenditure.setUserId(Integer.parseInt(userId));
 		expenditure.setPrice(Integer.parseInt(price));
 		expenditure.setDate(date);
 		expenditure.setItem(item);		
 		expenditure.setQuantity(Integer.parseInt(quantity));
 		expenditure.setTotalPrice(Integer.parseInt(totalPrice));
+		expenditureData = expenditure;		
 		
+		/*
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			// Convert object to JSON string and save into a file directly
+			mapper.writeValue(new File("D:\\expenditureData.json"), expenditureData);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		*/
 		
 		//invoke method to insert new row in expenditure table
 		ExpenditureDB.insert(expenditure);		
